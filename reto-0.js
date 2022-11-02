b41ec4dd8a883be18f09038bd0407bfc4035f1dd
Diego Seitler <diego_seitler@hotmail.com>
2022-11-02 20:32:42 -0300

    reto #0

---
+++ reto-0.js
@@ -0,0 +1,17 @@
+// Fizz Buzz
+
+function fizzBuzz () {
+    for(let i = 1; i <= 100; i++)
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fizz Buzz");
+    }
+      else if (i == 3 || i % 3 == 0) {
+        console.log("Fizz");
+    } else if(i == 5 || i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+fizzBuzz()
